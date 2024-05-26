f788ab35ff0c0690147126c0066d0b3013028343
Ashutosh <kumarashutosh0506@gmail.com>
2024-05-26 16:34:48 +0530

    Adding All Files

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Ashutosh Kumar",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/C4E03AQFETfHeS-tpwg/profile-displayphoto-shrink_200_200/0/1663839748834?e=2147483647&v=beta&t=rLIlB05rjZXXqjewL7BDj-OWEBkt95so59S0s0Dbak8",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Rohit Raj",
+    job: "Graphic Designer",
+    image: "https://media.licdn.com/dms/image/C4D03AQGLkfxnerVyAg/profile-displayphoto-shrink_200_200/0/1663993905721?e=2147483647&v=beta&t=vuW_py_TneI09gbaqNJ9pYxQVC9bY-M2veYHrfd5LyQ",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Swapnil Titoo",
+    job: "Marketing Manager",
+    image: "https://media.licdn.com/dms/image/D5603AQHIR-Qen73ocg/profile-displayphoto-shrink_800_800/0/1680037348439?e=2147483647&v=beta&t=lFmFTMMMevPp7LVfXA0zgN2027oAZRoxVp6QkNleEOM",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Akshat Awasthi",
+    job: "Content Strategist",
+    image: "https://media.licdn.com/dms/image/D4D03AQFeyCSqjEnRYA/profile-displayphoto-shrink_800_800/0/1683719199976?e=2147483647&v=beta&t=Eyg-EhvwXaRhspcLUINIzEFC0TykMQ3fFhPQnFLNa2I",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Golu Kumar",
+    job: "Data Analyst",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_zgwbCLkILecRNuWbo6vk7t2qRichLCva7fMMm_5YSw&s",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+  {
+    id: 6,
+    name: "Mohit Raj",
+    job: "Data Analyst",
+    image: "https://media.licdn.com/dms/image/D4D03AQFPXCAfjG5vZA/profile-displayphoto-shrink_800_800/0/1713090122166?e=2147483647&v=beta&t=9x9xN0FJafQLIG5x6pE0izVR2HWBWgybXFtlxcwyZnM",
+    text: "Working with this team on our data analysis project was excellent. They demonstrated outstanding analytical skills and attention to detail, handling complex datasets with ease. Their insights were crucial in shaping our business strategy. Highly professional and collaborative, they made the process smooth and productive. I highly recommend them.",
+  },
+  {
+    id: 7,
+    name: "Mohit Sharma",
+    job: "Data Analyst",
+    image: "https://media.licdn.com/dms/image/C4D03AQGhgxoxQ8CYpA/profile-displayphoto-shrink_800_800/0/1641487463824?e=2147483647&v=beta&t=LaEuyizigt6kTs_5J4MEBsUeJqPK8kMOiwdG4SZsf_8",
+    text: "Collaborating with this team on a data analysis project was a great experience. Their expertise with complex data sets and ability to provide clear insights significantly influenced our business planning. Their meticulous approach and professionalism made the project enjoyable and successful. I would work with them again.",
+  },
+  {
+    id: 8,
+    name: "Rupesh Sharma",
+    job: "Data Analyst",
+    image: "https://media.licdn.com/dms/image/D4D03AQEvzM7it8GUZA/profile-displayphoto-shrink_800_800/0/1684391141325?e=2147483647&v=beta&t=Q_AsZfw_nFOqiQlN2CnQm84t8_OFrVpIpvGnJh2alZ0",
+    text: "This team’s work on our data analysis project was exceptional. Their attention to detail and skill with complex datasets helped us uncover critical insights. They presented findings clearly and were highly professional throughout. I highly recommend their services for high-quality data analysis.",
+  },
+];
+
+export default reviews;
